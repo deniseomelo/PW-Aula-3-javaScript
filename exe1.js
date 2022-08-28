d906fa9752ccccbfd54a537f0d2b8a920be4faf1
Denise Melo <nissemello@gmail.com>
2022-08-28 11:34:37 -0300

    commit

---
+++ exe1.js
@@ -0,0 +1,14 @@
+/*  SENAC-TADS-Prrogramação Web
+        Exemplo de função */
+
+ // Aqui decçaramos a função  
+ function somar(parcela1, parcela2) {
+    let resultado = parcela1 + parcela2;
+    return resultado;
+
+ }  
+ let valor1 = 3;
+ let valor2 = 6
+ somaDosValores = somar(valor1, valor2);
+ //vamos chamar a função e mostar o resultado
+ console.log(`O resultado da soma de ${valor1} + ${valor2} = ${somaDosValores} `);  
