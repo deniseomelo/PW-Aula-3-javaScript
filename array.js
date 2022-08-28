763c13af807a4b5da80a6ae94385b61b3de697ca
Denise Melo <nissemello@gmail.com>
2022-08-28 11:59:06 -0300

    commit

---
+++ array.js
@@ -0,0 +1,8 @@
+/*  SENAC-TADS-Prrogramação Web
+        Exemplo de função */
+
+       //declarando array
+       
+       const listaDeProdutos = ["camisa, chinelos , galochas, metacapacetes, luvas"];
+       // aqui estamos a mostar o conteudo de array
+       console.log("listas de produtos:\n" + listasDeProdutos);
