5cd58c8136ba5c325a982a188a5e0b2a89eef679
Denise Melo <nissemello@gmail.com>
2022-08-28 11:51:34 -0300

    commit

---
+++ 2_arrow_1.js
@@ -0,0 +1,23 @@
+**---------------------------------------------------------- --*
+*SENAC - TADS - Programação Web*                
+* Exemplo de função Arrow *
+*------------------------------------------------- -*/
+//
+// vamos chamar a função e mostrar o resultado
+deixe  valor1 = 3 ;
+deixe  valor2 = 6
+let  somaDosValores  =  ( parcela1 ,  parcela2 )  =>  {
+    let  resultado  =  parcela1 + parcela2 ;
+    retorno  resultado ;
+} ;
+ 
+//console.log(`O resultado da soma de ${valor1} + ${valor2} =${somaDosValores(valor1,valor2)} `);
+
+
+
+//exemplo com 1 so parametro
+nomeCliente = "Joaquim José da Silva Xavier" ;
+let  nmcli  =  parmNomeCliente  =>  {
+    consola . log ( "Bem vindo Sr." + parmNomeCliente )
+}
+nmcli ( nomeCliente ) ;
